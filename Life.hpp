eacb58cc6c492c35b7860e66555b3295ba6ceea6
Sante Huhhuh <hyrskynmyrsky@gmail.com>
2013-10-03 01:31:02 +0300

    Fixed memory leak

--- Life.hpp
+++ Life.hpp
@@ -5,6 +5,10 @@
 #include "Map.hpp"
 #include "Rules.hpp"
 
+struct Point3D {
+	int x, y, z;
+};
+
 class Life {
 private:
 public:
@@ -12,53 +16,59 @@ public:
 		printf("Life constructed\n");
 	}
 	void update(Map *map) {
-		Map *sourcemap = new Map(*map);
+		std::vector<Point3D> lifeDelta;
 
-		for(int z = 0; z < sourcemap->getDepth(); z++) {
-		for(int y = 0; y < sourcemap->getHeight(); y++) {
-		for(int x = 0; x < sourcemap->getWidth(); x++) {
+		for(int z = 0; z < map->getDepth(); z++) {
+		for(int y = 0; y < map->getHeight(); y++) {
+		for(int x = 0; x < map->getWidth(); x++) {
 
 			int neighbours = 0;
 
-			int xp = sourcemap->X(x+1);
-			int xm = sourcemap->X(x-1);
-			int yp = sourcemap->Y(y+1);
-			int ym = sourcemap->Y(y-1);
-			int zp = sourcemap->Z(z+1);
-			int zm = sourcemap->Z(z-1);
+			int xp = map->X(x+1);
+			int xm = map->X(x-1);
+			int yp = map->Y(y+1);
+			int ym = map->Y(y-1);
+			int zp = map->Z(z+1);
+			int zm = map->Z(z-1);
 
-			neighbours += sourcemap->getLife(xp, yp, zp);
-			neighbours += sourcemap->getLife(xp, y , zp);
-			neighbours += sourcemap->getLife(xp, ym, zp);
-			neighbours += sourcemap->getLife(x , yp, zp);
-			neighbours += sourcemap->getLife(x , y , zp);
-			neighbours += sourcemap->getLife(x , ym, zp);
-			neighbours += sourcemap->getLife(xm, yp, zp);
-			neighbours += sourcemap->getLife(xm, y , zp);
-			neighbours += sourcemap->getLife(xm, ym, zp);
-			neighbours += sourcemap->getLife(xp, yp, zm);
-			neighbours += sourcemap->getLife(xp, y , zm);
-			neighbours += sourcemap->getLife(xp, ym, zm);
-			neighbours += sourcemap->getLife(x , yp, zm);
-			neighbours += sourcemap->getLife(x , y , zm);
-			neighbours += sourcemap->getLife(x , ym, zm);
-			neighbours += sourcemap->getLife(xm, yp, zm);
-			neighbours += sourcemap->getLife(xm, y , zm);
-			neighbours += sourcemap->getLife(xm, ym, zm);
-			neighbours += sourcemap->getLife(xp, yp, z);
-			neighbours += sourcemap->getLife(xp, y , z);
-			neighbours += sourcemap->getLife(xp, ym, z);
-			neighbours += sourcemap->getLife(x , yp, z);
-			neighbours += sourcemap->getLife(x , ym, z);
-			neighbours += sourcemap->getLife(xm, yp, z);
-			neighbours += sourcemap->getLife(xm, y , z);
-			neighbours += sourcemap->getLife(xm, ym, z);
+			neighbours += map->getLife(xp, yp, zp);
+			neighbours += map->getLife(xp, y , zp);
+			neighbours += map->getLife(xp, ym, zp);
+			neighbours += map->getLife(x , yp, zp);
+			neighbours += map->getLife(x , y , zp);
+			neighbours += map->getLife(x , ym, zp);
+			neighbours += map->getLife(xm, yp, zp);
+			neighbours += map->getLife(xm, y , zp);
+			neighbours += map->getLife(xm, ym, zp);
+			neighbours += map->getLife(xp, yp, zm);
+			neighbours += map->getLife(xp, y , zm);
+			neighbours += map->getLife(xp, ym, zm);
+			neighbours += map->getLife(x , yp, zm);
+			neighbours += map->getLife(x , y , zm);
+			neighbours += map->getLife(x , ym, zm);
+			neighbours += map->getLife(xm, yp, zm);
+			neighbours += map->getLife(xm, y , zm);
+			neighbours += map->getLife(xm, ym, zm);
+			neighbours += map->getLife(xp, yp, z);
+			neighbours += map->getLife(xp, y , z);
+			neighbours += map->getLife(xp, ym, z);
+			neighbours += map->getLife(x , yp, z);
+			neighbours += map->getLife(x , ym, z);
+			neighbours += map->getLife(xm, yp, z);
+			neighbours += map->getLife(xm, y , z);
+			neighbours += map->getLife(xm, ym, z);
 
-			if (sourcemap->getLife(x, y, z) == true)
-			     map->setLife(x, y, z, (rules::willStayAlive(neighbours)));
-			else map->setLife(x, y, z, (rules::willReincarnate(neighbours)));
+			if (map->getLife(x, y, z) == true && rules::willDie(neighbours)) {
+				lifeDelta.emplace_back(Point3D{ x, y, z });
+			} else if (map->getLife(x, y, z) == false && (rules::willReincarnate(neighbours))) {
+				lifeDelta.emplace_back(Point3D{ x, y, z });
+			}
 		}
 		}
+		}
+
+		for ( const Point3D& p : lifeDelta ) {
+			map->setLife(p.x, p.y, p.z, !map->getLife(p.x, p.y, p.z));	
 		}
 	}
 };
